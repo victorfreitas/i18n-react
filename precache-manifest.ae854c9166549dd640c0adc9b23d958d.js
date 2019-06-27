ef46da610017506a23373f57e8993cc6f4038066
Victor Freitas <victorfreitasdev@gmail.com>
2019-06-27 13:02:36 -0300

    Updates

---
+++ precache-manifest.ae854c9166549dd640c0adc9b23d958d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4536ea8d396308cd23a239c4225791bf",
+    "url": "/react-i18n/index.html"
+  },
+  {
+    "revision": "41a700b8356957cdc8e2",
+    "url": "/react-i18n/static/css/main.b8cb500e.chunk.css"
+  },
+  {
+    "revision": "e7a975eb4bc81f4bd289",
+    "url": "/react-i18n/static/js/2.5fab942d.chunk.js"
+  },
+  {
+    "revision": "41a700b8356957cdc8e2",
+    "url": "/react-i18n/static/js/main.96e91d34.chunk.js"
+  },
+  {
+    "revision": "98545c65ddf811fefe23",
+    "url": "/react-i18n/static/js/runtime~main.80c18ee2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-i18n/static/media/logo.5d5d9eef.svg"
+  }
+]);
